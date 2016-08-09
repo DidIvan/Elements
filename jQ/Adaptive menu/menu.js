43dfbadf6b7e5904f8e2cc3afcac7a10549ec4aa
Ivan <Holy.vinil@gmail.com>
2016-08-09 16:58:21 +0300

    adaptive menu

--- jQ/Adaptive menu/menu.js
+++ jQ/Adaptive menu/menu.js
@@ -2,11 +2,13 @@
  * Created by Horbynko on 09.08.2016.
  */
 $(document).ready(function () {
-   $('.menu-trigger').on('click',function () {
-       $('nav li').slideToggle(500)
-   });
+    $('.menu-trigger').on('click', function () {
+        $('nav li').slideToggle(500)
+    });
     /*after using slidetoggle - necessary to remove style ="display:none"*/
     $(window).resize(function () {
-        $('nav li').removeAttr('style')
+        if ($(window).width() > 500) {
+            $('nav li').removeAttr('style')
+        }
     })
 });
