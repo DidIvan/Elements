4aa03136d196ff1c0292a8496c2be3b64cbe8f33
Ivan <Holy.vinil@gmail.com>
2016-08-09 17:02:06 +0300

    adaptive menu

--- jQ/Adaptive menu/menu.js
+++ jQ/Adaptive menu/menu.js
@@ -5,7 +5,9 @@ $(document).ready(function () {
     $('.menu-trigger').on('click', function () {
         $('nav li').slideToggle(500)
     });
-    /*after using slidetoggle - necessary to remove style ="display:none"*/
+    /*after using slidetoggle - necessary to remove style ="display:none";
+    as an alternative we can write down this string in style.css -
+    @media (min-width: 501px) { nav ul { display: block !important; } }﻿*/
     $(window).resize(function () {
         if ($(window).width() > 500) {
             $('nav li').removeAttr('style')
