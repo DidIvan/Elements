2a354a06f345bb60709226b1c79586c4a4f6c27d
Ivan <Holy.vinil@gmail.com>
2017-03-14 21:17:22 +0200

    slider on pure js

---
+++ Pure JS/slider/slider.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+    const prev = document.querySelector('.prev');
+    const next = document.querySelector('.next');
+    const image = document.querySelectorAll('.photos img');
+    let i = 0;
+    console.log(image)
+    prev.onclick = function () {
+        image[i].className = '';
+        i--;
+        if (i < 0) {
+            i = image.length - 1;
+        }
+        image[i].className = "showed"
+    };
+    next.onclick = function () {
+        image[i].className = '';
+        i++;
+        if (i >= image.length) {
+            i = 0;
+        }
+        image[i].className = "showed"
+
+    }
+};
