ec6d593bb6a0547a2b676cae5749f7ee90cd670c
Ivan <horbynko.i@gmail.com>
2018-08-26 22:27:42 +0300

    modificate dropdown menu

--- jQ/dropmenu/dropmenu.js
+++ jQ/dropmenu/dropmenu.js
@@ -1,10 +1,9 @@
 $(document).ready(function () {
-    $('#slow_nav  ul li').hover(
-        function () {
-            $('ul', this).stop().slideDown(400);
+    $('#slow_nav  ul li').hover( function () {
+            $('.dropdown', this).stop().slideDown(400);
         },
         function () {
-            $('ul', this).stop().slideUp(400);
+            $('.dropdown', this).stop().slideUp(400);
         }
     );
 });
