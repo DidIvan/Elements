e2d90423b0355efe0b48f2121c5efa84a067e324
Ivan <horbynko.i@gmail.com>
2018-08-26 20:19:32 +0300

    created dropdown menu

---
+++ jQ/dropmenu/dropmenu.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#slow_nav  ul li').hover(
+        function () {
+            $('ul', this).stop().slideDown(400);
+        },
+        function () {
+            $('ul', this).stop().slideUp(400);
+        }
+    );
+});
